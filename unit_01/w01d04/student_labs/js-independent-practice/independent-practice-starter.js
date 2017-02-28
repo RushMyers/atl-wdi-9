5e3fad8d2d6bfdea94000bc790e881bafde4e01d
RushMyers <myersrush1986@gmail.com>
2017-02-28 16:54:48 -0500

    Update independent-practice-starter.js

--- unit_01/w01d04/student_labs/js-independent-practice/independent-practice-starter.js
+++ unit_01/w01d04/student_labs/js-independent-practice/independent-practice-starter.js
@@ -3,6 +3,19 @@
 // number is the length of the corresponding string.
 
 // your code here
+    var arr = ['apples', 'oranges', 'bananas']; 
+
+   var lengths = function(arrayOfStrings) { 
+     var arrayOfNumbers = []; 
+     for(i=0; i<arrayOfStrings.length: i++) { 
+       var lengthOfStrings = [i]length; 
+       
+     } 
+     return arrayOfNumbers; 
+   } 
+    lengths(arr); 
+   
+   
 
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
@@ -11,10 +24,29 @@
 // The transmogrified result of three numbers is the product of the first two numbers,
 // raised to the power of the third number.
 
-// your code here
+// 
 
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
-// your code here
+// function wordReverse(anyString) { 
+    function wordReverse (stringToReverse) { 
+  var arrayOfWords = stringToReverse.split(''); 
+  var reversedWords = []; 
+  reversedWords=arrayOfWords.reverse(); 
+  return reversedWords.join(''); 
+}
+//Or: 
+
+function wordReverse (stringToReverse) { 
+  var arrayOfWords = stringToReverse.split(''); 
+  
+  var reversedWords = arrayOfWords.reverse(); 
+  
+  return reversedWords.join(''); 
+} 
+
+someString = "I've got a lovely bunch of coconuts"; 
+
+wordReverse(someString); 
