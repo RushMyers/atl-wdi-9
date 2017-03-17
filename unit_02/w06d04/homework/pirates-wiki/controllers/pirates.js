b3a9f7013f62c6d37ff8ef3e5adde8dbc40b9ced
myersRush <myersrush1986@gmail.com>
2017-03-17 07:48:35 -0400

    tried to do pirates homework

--- unit_02/w06d04/homework/pirates-wiki/controllers/pirates.js
+++ unit_02/w06d04/homework/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,26 @@
-//your code below
+var express = require('express');
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+// router.get('/', function(req, res) {
+//   res.send('index', {
+//   var pirateNames = [];
+//   for(var i = 0; i < pirates.length; i++) {
+//   pirateNames.push(pirates[i].name)
+//   console.log(pirateNames);
+//     };
+//   });
+// });
+
+// router.get('/:id;', function(req, res) {
+
+// });
+
+// router.post('/new', function(req, res) {
+
+// });
+
+
+
+
+module.exports = router;
--- unit_02/w06d04/homework/pirates-wiki/server.js
+++ unit_02/w06d04/homework/pirates-wiki/server.js
@@ -1,1 +1,23 @@
-//your code here
+var express = require('express');
+var bodyParser = require('body-parser');
+var handleBars = require('hbs');
+var morgan = require('morgan');
+var app = express();
+var port = process.env.PORT || 3000;
+var methodOverride = require('method-override');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.get('/', function(req, res) {
+  res.send('whattup pirate');
+});
+
+var piratesController = require(__dirname + '/controllers/pirates.js');
+
+app.use('/pirates', piratesController);
+
+
+app.listen(port, function() {
+  console.info('servin it up', port, "//", new Date());
+});
